cd3244486a71f0649fac8e5331ab5baaddcb72aa
heavy-stone <44592900+hevy-stone@users.noreply.github.com>
2024-01-31 15:43:05 +0900

    add constants for score

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+STRIKE_SCORE = 10
+ZERO_SCORE = 0
 LAST_FRAME = 9
 SECOND_FRAME_FROM_THE_LAST = 8
 
@@ -9,8 +11,8 @@ scores = score_args.split(',')
 shots = []
 scores.each do |score|
   if score == 'X'
-    shots << 10
-    shots << 0
+    shots << STRIKE_SCORE
+    shots << ZERO_SCORE
   else
     shots << score.to_i
   end
@@ -21,27 +23,27 @@ shots.each_slice(2).with_index(0) do |shot, i|
   if i < LAST_FRAME
     frames << shot
   elsif i == LAST_FRAME
-    shot -= [0] if shot[0] == 10
+    shot -= [0] if shot[0] == STRIKE_SCORE
     frames << shot
   elsif i > LAST_FRAME
-    shot -= [0] if shot[0] == 10
+    shot -= [0] if shot[0] == STRIKE_SCORE
     frames.last.push(*shot)
   end
 end
 
 point_frames = []
 frames.each_with_index do |frame, i|
-  if frame[0] == 10 && i < SECOND_FRAME_FROM_THE_LAST
-    if frames[(i + 1)][0] == 10
-      frame.push(10)
+  if frame[0] == STRIKE_SCORE && i < SECOND_FRAME_FROM_THE_LAST
+    if frames[(i + 1)][0] == STRIKE_SCORE
+      frame.push(STRIKE_SCORE)
       frame.push(frames[i + 2][0])
     else
       frame.push(*frames[i + 1])
     end
-  elsif frame[0] == 10 && i == SECOND_FRAME_FROM_THE_LAST
+  elsif frame[0] == STRIKE_SCORE && i == SECOND_FRAME_FROM_THE_LAST
     frame.push(frames[i + 1][0])
     frame.push(frames[i + 1][1])
-  elsif frame.sum == 10 && i < LAST_FRAME
+  elsif frame.sum == STRIKE_SCORE && i < LAST_FRAME
     frame.push(frames[(i + 1)][0])
   end
   point_frames.push(frame)
