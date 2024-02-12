667ef2bd90de852bf5bc9d2a60fd4ef3272dee13
heavy-stone <44592900+hevy-stone@users.noreply.github.com>
2024-02-12 19:56:23 +0900

    add path option

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -5,33 +5,65 @@ require 'optparse'
 
 WHITESPACE_BETWEEN_FILE_NAMES = 2
 DISPLAYED_COLUMN = 3
+CURRENT_DIRECTORY = '.'
 
 def main
   options = ARGV.getopts('a')
-  puts ls(options)
+  paths = ARGV.empty? ? [CURRENT_DIRECTORY] : ARGV
+  print ls(options, paths)
 end
 
-def ls(options)
+def ls(options, paths = [CURRENT_DIRECTORY])
+  valid_paths = validate_paths(paths)
+  create_output(valid_paths, each_path_filenames(options, valid_paths))
+end
+
+def validate_paths(paths)
+  paths.sort.filter do |path|
+    unless Dir.exist?(path)
+      puts "ls: #{path}: No such file or directory"
+      next
+    end
+    path
+  end
+end
+
+def each_path_filenames(options, paths)
   if options['a']
-    create_output(Dir.entries('.'))
+    paths.map { |path| Dir.entries(path) }
   else
-    create_output(Dir.glob('*'))
+    paths.map { |path| Dir.glob('*', base: path) }
   end
 end
 
-def create_output(input_file_names)
-  ljust_width = input_file_names.max_by(&:length).length + WHITESPACE_BETWEEN_FILE_NAMES
-  column = input_file_names.length.ceildiv(DISPLAYED_COLUMN)
-  sliced_input_file_names = input_file_names.sort.each_slice(column).to_a
-  nil_count_for_transpose = column - sliced_input_file_names[-1].length
-  nil_count_for_transpose.times do
-    sliced_input_file_names[-1].push(nil)
+def create_output(paths, each_path_filenames)
+  if paths.length > 1
+    paths.zip(each_path_filenames).map do |path, filenames|
+      next "#{path}:\n" if filenames.empty?
+
+      "#{path}:\n#{create_filenames(filenames)}"
+    end.join("\n")
+  else
+    filenames = each_path_filenames.flatten
+    return '' if filenames.empty?
+
+    create_filenames(filenames)
   end
-  sliced_input_file_names.transpose.map do |row|
+end
+
+def create_filenames(filenames)
+  ljust_width = filenames.max_by(&:length).length + WHITESPACE_BETWEEN_FILE_NAMES
+  column = filenames.length.ceildiv(DISPLAYED_COLUMN)
+  sliced_filenames = filenames.sort.each_slice(column).to_a
+  nil_count_for_transpose = column - sliced_filenames[-1].length
+  nil_count_for_transpose.times do
+    sliced_filenames[-1].push(nil)
+  end
+  sliced_filenames.transpose.map do |row|
     row.map do |file_name|
       file_name.to_s.ljust(ljust_width)
     end.join
-  end.join("\n")
+  end.join("\n").concat("\n")
 end
 
 main if $PROGRAM_NAME == __FILE__
