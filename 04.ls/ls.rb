df4956b8c2cdf9276018609712bc0d9cffe2de95
heavy-stone <44592900+hevy-stone@users.noreply.github.com>
2024-02-15 16:39:29 +0900

    add r option and full width file name support

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -3,19 +3,23 @@
 
 require 'optparse'
 
-WHITESPACE_BETWEEN_FILE_NAMES = 2
+WHITESPACE_BETWEEN_FILENAMES = 2
 DISPLAYED_COLUMN = 3
 CURRENT_DIRECTORY = '.'
+FULL_WIDTH_REGEX = /[^ -~｡-ﾟ]/
 
 def main
-  options = ARGV.getopts('a')
+  options = ARGV.getopts('ar')
   paths = ARGV.empty? ? [CURRENT_DIRECTORY] : ARGV
   print ls(options, paths)
 end
 
 def ls(options, paths = [CURRENT_DIRECTORY])
   valid_paths = validate_paths(paths)
-  create_output(valid_paths, each_path_filenames(options, valid_paths))
+  valid_paths = valid_paths.reverse if options['r']
+  each_path_filenames = each_path_filenames(options, valid_paths)
+  each_path_filenames = each_path_filenames.map(&:reverse) if options['r']
+  create_output(valid_paths, each_path_filenames)
 end
 
 def validate_paths(paths)
@@ -30,9 +34,9 @@ end
 
 def each_path_filenames(options, paths)
   if options['a']
-    paths.map { |path| Dir.entries(path) } # ..の表示を含めるためentriesを使用
+    paths.map { |path| Dir.entries(path).sort } # ..の表示を含めるためentriesを使用
   else
-    paths.map { |path| Dir.glob('*', base: path) }
+    paths.map { |path| Dir.glob('*', base: path).sort }
   end
 end
 
@@ -49,16 +53,23 @@ end
 def create_filenames(filenames)
   return if filenames.empty?
 
-  ljust_width = filenames.max_by(&:length).length + WHITESPACE_BETWEEN_FILE_NAMES
+  max_length_filename = filenames.max_by do |filename|
+    filename.length + filename.scan(FULL_WIDTH_REGEX).count # 全角文字は2文字としてカウントするため
+  end
+  ljust_width =
+    max_length_filename.length +
+    max_length_filename.scan(FULL_WIDTH_REGEX).count +
+    WHITESPACE_BETWEEN_FILENAMES
   column = filenames.length.ceildiv(DISPLAYED_COLUMN)
-  sliced_filenames = filenames.sort.each_slice(column).to_a
+  sliced_filenames = filenames.each_slice(column).to_a
   nil_count_for_transpose = column - sliced_filenames[-1].length
   nil_count_for_transpose.times do
     sliced_filenames[-1].push(nil)
   end
   sliced_filenames.transpose.map do |row|
-    row.map do |file_name|
-      file_name.to_s.ljust(ljust_width)
+    row.map do |filename|
+      filename = filename.to_s if filename.nil?
+      filename.ljust(ljust_width - filename.scan(FULL_WIDTH_REGEX).count)
     end.join
   end.join("\n").concat("\n")
 end
