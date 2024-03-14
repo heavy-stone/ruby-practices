65e8c4426df0702f226e74027ddc4205b84b6e7e
heavy-stone <44592900+hevy-stone@users.noreply.github.com>
2024-03-14 17:37:21 +0900

    rename method names

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -13,15 +13,15 @@ end
 def wc(options, files = [])
   if files.empty?
     lines = $stdin.readlines
-    file_content_counts = [create_file_field_hash_with_lines('', lines)]
+    file_content_counts = [to_file_content_counts('', lines)]
   else
-    file_content_counts = files.map { |file| create_file_field_hash(file) }
+    file_content_counts = files.map { |file| to_file_content_counts_from_filename(file) }
   end
-  total_field_hash = create_total_field_hash(file_content_counts)
-  create_output(file_content_counts, total_field_hash, options)
+  total_file_content_counts = to_total_file_content_counts(file_content_counts)
+  format_counts_group(file_content_counts, total_file_content_counts, options)
 end
 
-def create_file_field_hash(file)
+def to_file_content_counts_from_filename(file)
   error_message =
     if File.directory?(file)
       "wc: #{file}: read: Is a directory\n"
@@ -33,45 +33,45 @@ def create_file_field_hash(file)
 
   if error_message.empty?
     lines = File.readlines(file)
-    create_file_field_hash_with_lines(file, lines)
+    to_file_content_counts(file, lines)
   else
-    create_file_field_hash_with_lines('', [], error_message)
+    to_file_content_counts('', [], error_message)
   end
 end
 
-def create_file_field_hash_with_lines(file, lines, error_message = '')
+def to_file_content_counts(file, lines, error_message = '')
   words = lines.join.split
   bytes = lines.join.bytesize
   { line_length: lines.length, word_length: words.length, bytes:, label: file, error_message: }
 end
 
-def create_total_field_hash(file_content_counts)
+def to_total_file_content_counts(file_content_counts)
   line_length = file_content_counts.sum { |file| file[:line_length] }
   word_length = file_content_counts.sum { |file| file[:word_length] }
   bytes = file_content_counts.sum { |file| file[:bytes] }
   { line_length:, word_length:, bytes:, label: 'total' }
 end
 
-def create_output(file_content_counts, total_field_hash, options)
-  output = file_content_counts.map do |file_field_hash|
-    if file_field_hash[:error_message].empty?
-      format_output(file_field_hash, total_field_hash, options)
+def format_counts_group(file_content_counts, total_file_content_counts, options)
+  output = file_content_counts.map do |file_content_count|
+    if file_content_count[:error_message].empty?
+      format_counts(file_content_count, total_file_content_counts, options)
     else
-      file_field_hash[:error_message]
+      file_content_count[:error_message]
     end
   end.join
-  output += format_output(total_field_hash, total_field_hash, options) if file_content_counts.length > 1
+  output += format_counts(total_file_content_counts, total_file_content_counts, options) if file_content_counts.length > 1
   output
 end
 
-def format_output(file_field_hash, total_field_hash, options)
+def format_counts(file_content_count, total_file_content_counts, options)
   key_to_has_option = { line_length: options['l'], word_length: options['w'], bytes: options['c'] }
-  total_field_hash.map do |key, value|
+  total_file_content_counts.map do |key, value|
     if key == :label
-      " #{file_field_hash[:label]}\n"
+      " #{file_content_count[:label]}\n"
     else
       width = value.to_s.length
-      "\t#{file_field_hash[key].to_s.rjust(width)}" if key_to_has_option[key]
+      "\t#{file_content_count[key].to_s.rjust(width)}" if key_to_has_option[key]
     end
   end.join
 end
